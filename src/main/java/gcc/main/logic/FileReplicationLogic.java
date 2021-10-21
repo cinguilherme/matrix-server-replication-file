39515e4181c43814354ea845abb245b75de477ab
Cintra Guilherme <cinguilherme@gmail.com>
2021-10-21 07:53:06 -0300

    fail for big scenario, trap worked

--- src/main/java/gcc/main/logic/FileReplicationLogic.java
+++ src/main/java/gcc/main/logic/FileReplicationLogic.java
@@ -1,9 +1,6 @@
 package gcc.main.logic;
 
 import org.javatuples.Pair;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 final public class FileReplicationLogic {
@@ -26,7 +23,4 @@ final public class FileReplicationLogic {
 
         return count;
     }
-
-
-
 }
--- src/test/java/gcc/main/logic/FileReplicationLogicTest.java
+++ src/test/java/gcc/main/logic/FileReplicationLogicTest.java
@@ -6,29 +6,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileReplicationLogicTest {
 
-    private int[][] zeroMatrix() {
-        var matrix = new int[3][3];
-        matrix[0][0] = 0;
-        matrix[0][1] = 0;
-        matrix[0][2] = 0;
-        matrix[1][0] = 0;
-        matrix[1][1] = 0;
-        matrix[1][2] = 0;
-        matrix[2][0] = 0;
-        matrix[2][1] = 0;
-        matrix[2][2] = 0;
+    private int[][] zeroMatrix(int size) {
+        var matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+
         return matrix;
     }
 
     @Test
     void shouldBe2() {
 
-        var matrix = zeroMatrix();
+        var matrix = zeroMatrix(3);
         matrix[1][1] = 1;
 
         int actual = FileReplicationLogic.ticksTillReplicationComplete(matrix);
 
         assertEquals(2, actual);
+    }
+
+    @Test
+    void shouldBe2000(){
+        int[][] matrix = zeroMatrix(2000);
+
+        matrix[1000][1000] = 1;
+
+        int actual = FileReplicationLogic.ticksTillReplicationComplete(matrix);
+
+        assertEquals(2000, actual);
     }
 
 }
